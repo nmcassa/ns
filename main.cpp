7eae39070460764fca57a323514947ceb7309bb1
nmcassa <nmcassa804@outlook.com>
2022-08-02 10:53:14 -0400

    cpp maybe

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream> //read file
+#include <stdexcept> //errors
+#include <sstream>
+#include <vector>
+using namespace std;
+
+vector<string> splitString(string str) {
+  stringstream ss(str);
+  string to;
+  vector<string> code;
+
+  while (getline(ss, to, '\n')) {
+    code.push_back(to);
+  }
+
+  return code;
+}
+
+int main() {
+  string myText;
+  string total;
+
+  ifstream MyReadFile("hello.nick");
+
+  // Use a while loop together with the getline() function to read the file line by line
+  while (getline (MyReadFile, myText)) {
+    total = total + myText + "\n";
+  }
+
+  MyReadFile.close();
+
+  vector<string> code = splitString(total);
+
+  cout << code.at(4) << endl << code.at(5) << endl;
+
+  return 0;
+}
